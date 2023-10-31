6397ad9c4da0b0d17524ab0bbd6942afcff32755
Jyothi <73483399+Jyothik6@users.noreply.github.com>
2023-10-31 17:50:41 +0530

    Add files via upload

---
+++ public/src/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+class App extends Component {
+  render(){
+
+  
+  return (
+    <div className="App">
+      <center><b>component life cycle example</b></center>
+    </div>
+  );
+}
+}
+export default App;
